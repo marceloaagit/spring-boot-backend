691a230a44a3c23a8bffed1860bb815ee9a74f90
marceloaagit <marcelo.maa@pc.ms.gov.br>
2019-03-20 21:50:47 -0400

    teste de git

--- src/main/java/com/marceloalves/backend/CursomcApplication.java
+++ src/main/java/com/marceloalves/backend/CursomcApplication.java
@@ -108,7 +108,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Pagamento pag1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pag1);
 		
-		Pagamento pag2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pag2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2018 00:00"), null);
 		ped2.setPagamento(pag2);
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
