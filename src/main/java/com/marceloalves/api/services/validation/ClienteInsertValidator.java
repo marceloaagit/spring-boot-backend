9cfb7e72776273dbd7e4d2685dfd289f86a189b5
marceloaagit <marcelo.maa@pc.ms.gov.br>
2019-03-29 19:09:31 -0400

    validação para não inserir email repetidos

--- src/main/java/com/marceloalves/api/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marceloalves/api/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.marceloalves.api.controllers.exception.FieldMessage;
 import com.marceloalves.api.dtos.ClienteNewDTO;
+import com.marceloalves.api.entities.Cliente;
 import com.marceloalves.api.entities.enums.TipoCliente;
+import com.marceloalves.api.repositories.ClienteRepository;
 import com.marceloalves.api.services.validation.utils.CpfOuCnjpValidUtil;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -19,18 +27,25 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
 		
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		// inclua os testes aqui, inserindo erros na lista
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !CpfOuCnjpValidUtil.isValidCpf(objDto.getCpfOuCnpj())) {
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())
+				&& !CpfOuCnjpValidUtil.isValidCpf(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido!!!"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !CpfOuCnjpValidUtil.isValidCnpj(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
+				&& !CpfOuCnjpValidUtil.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!!!"));
 		}
 		
+		Cliente clienteEmail = clienteRepository.findByEmail(objDto.getEmail());
+		if(clienteEmail != null)
+			list.add(new FieldMessage("email", "Email já cadastrado!!!"));
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
