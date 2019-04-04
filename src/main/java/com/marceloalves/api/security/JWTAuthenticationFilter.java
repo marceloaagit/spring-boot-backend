9b7dc766923db798912ab992da408791ac1b7714
marceloaagit <marcelo.maa@pc.ms.gov.br>
2019-04-03 22:06:46 -0400

    Expondo o header Authorization (problema de Cors)

--- src/main/java/com/marceloalves/api/security/JWTAuthenticationFilter.java
+++ src/main/java/com/marceloalves/api/security/JWTAuthenticationFilter.java
@@ -49,12 +49,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	}
 
 	@Override
-	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse response, FilterChain chain,
 			Authentication auth) throws IOException, ServletException {
 
 		String username = ((UserSpringSecurity) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
-		res.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");//configurando o cors do backend
 	}
 
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
